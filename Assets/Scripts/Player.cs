b8d91fb53016e35913618e048f91374035927e07
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2022-11-18 21:31:29 +0900

    2

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,9 +12,14 @@ public class Player : MonoBehaviour
 
     bool shiftDown;
     bool spaceDown;
+    bool rDown;
+    
+
     bool isRun;
     bool isDodge;
+    
     Vector3 moveVec;
+    Vector3 dodgeVec;
     Animator anim;
     Rigidbody rigid;
     private void Awake()
@@ -35,6 +40,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Dodge();
+        Reload();
     }
 
     void GetInput()
@@ -43,11 +49,14 @@ public class Player : MonoBehaviour
         vAxis = Input.GetAxisRaw("Vertical");
         shiftDown = Input.GetButton("Walk");
         spaceDown = Input.GetButtonDown("Dodge");
+        rDown = Input.GetButtonDown("Reload");
     }
 
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        if (isDodge)
+            moveVec = dodgeVec;
         transform.position += moveVec * (shiftDown ? speed/2 : speed) * Time.deltaTime;
         anim.SetBool("isWalk", shiftDown);
         anim.SetBool("isRun", moveVec != Vector3.zero);
@@ -60,13 +69,14 @@ public class Player : MonoBehaviour
 
     void Dodge()
     {
-        if(spaceDown && !isDodge)
+        if(spaceDown && !isDodge && !shiftDown)
         {
+            dodgeVec = moveVec;
             speed *= 2;
             anim.SetTrigger("doDodge");
             isDodge = true;
 
-            Invoke("DodgeOut", 2f);
+            Invoke("DodgeOut", 0.5f);
         }
         
     }
@@ -75,6 +85,14 @@ public class Player : MonoBehaviour
     {
         speed /= 2;
         isDodge = false;
+    }
+
+    void Reload()
+    {
+        if(rDown && moveVec == Vector3.zero && !isDodge)
+        {
+            anim.SetTrigger("doReload");
+        }
     }
     void SetCamPosition()
     {
