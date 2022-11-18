32bfaaaac7520739982e6e77eb6eed2ebcb808eb
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2022-11-18 17:14:56 +0900

    1

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public GameObject CameraTr;
+    public float speed;
+
     float hAxis;
     float vAxis;
-    public bool isRun;
-    public float speed;
+
+    bool shiftDown;
+    bool spaceDown;
+    bool isRun;
+    bool isDodge;
     Vector3 moveVec;
     Animator anim;
+    Rigidbody rigid;
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
+        rigid = GetComponent<Rigidbody>();
     }
     void Start()
     {
@@ -23,21 +30,52 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        GetInput();
+        Move();
+        Turn();
+        Dodge();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
-        isRun = Input.GetButton("Run");
+        shiftDown = Input.GetButton("Walk");
+        spaceDown = Input.GetButtonDown("Dodge");
+    }
+
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        transform.position += moveVec * (shiftDown ? speed/2 : speed) * Time.deltaTime;
+        anim.SetBool("isWalk", shiftDown);
+        anim.SetBool("isRun", moveVec != Vector3.zero);
+    }
 
-        anim.SetBool("isWalk", moveVec != Vector3.zero);
-        anim.SetBool("isRun", isRun);
-
-        transform.position += moveVec * (isRun ? speed * 3 : speed) * Time.deltaTime;
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
-        
+    }
 
+    void Dodge()
+    {
+        if(spaceDown && !isDodge)
+        {
+            speed *= 2;
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 2f);
+        }
         
     }
 
+    void DodgeOut()
+    {
+        speed /= 2;
+        isDodge = false;
+    }
     void SetCamPosition()
     {
         Camera.main.transform.parent = CameraTr.transform;
