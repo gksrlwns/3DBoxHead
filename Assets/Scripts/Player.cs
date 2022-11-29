ef30eede589b46f31b352dd3a9d05f8bda0d0b87
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2022-11-29 19:35:23 +0900

    swap

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -16,15 +16,22 @@ public class Player : MonoBehaviour
     bool shiftDown;
     bool spaceDown;
     bool rDown;
+    bool s1Down;
+    bool s2Down;
+    bool s3Down;
     
 
     bool isRun;
     bool isDodge;
+    bool isSwap;
+
+    int equipWeaponIndex = -1;
     
     Vector3 moveVec;
     Vector3 dodgeVec;
     Animator anim;
     Rigidbody rigid;
+    GameObject nearObject;
     private void Awake()
     {
         anim = GetComponentInChildren<Animator>();
@@ -38,12 +45,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
         GetInput();
         Move();
         Turn();
         Dodge();
         Reload();
+        Swap();
     }
 
     void GetInput()
@@ -53,18 +60,51 @@ public class Player : MonoBehaviour
         shiftDown = Input.GetButton("Walk");
         spaceDown = Input.GetButtonDown("Dodge");
         rDown = Input.GetButtonDown("Reload");
+        s1Down = Input.GetButtonDown("Swap1");
+        s2Down = Input.GetButtonDown("Swap2");
+        s3Down = Input.GetButtonDown("Swap3");
     }
 
     void Move()
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
-        if (isDodge)
-            moveVec = dodgeVec;
+        if (isDodge) moveVec = dodgeVec;
+        if (isSwap) moveVec = Vector3.zero;
         transform.position += moveVec * (shiftDown ? speed/2 : speed) * Time.deltaTime;
         anim.SetBool("isWalk", shiftDown);
         anim.SetBool("isRun", moveVec != Vector3.zero);
     }
 
+    void Swap()
+    {
+        if (s1Down && (!hasweapons[0] || equipWeaponIndex == 0)) return;
+        if (s2Down && (!hasweapons[1] || equipWeaponIndex == 1)) return;
+        if (s3Down && (!hasweapons[2] || equipWeaponIndex == 2)) return;
+
+        int weaponIndex = -1;
+        if (s1Down && hasweapons[0]) weaponIndex = 0;
+        if (s2Down && hasweapons[1]) weaponIndex = 1;
+        if (s3Down && hasweapons[2]) weaponIndex = 2;
+        if((s1Down || s2Down || s3Down) && !isDodge)
+        {
+            for(int i = 0; i < weapons.Length; i++)
+            {
+                weapons[i].SetActive(false);
+            }
+            equipWeaponIndex = weaponIndex;
+            weapons[weaponIndex].SetActive(true);
+            
+            anim.SetTrigger("doSwap");
+            isSwap = true;
+            Invoke("SwapOut", 0.5f);
+        }
+        
+    }
+
+    void SwapOut()
+    {
+        isSwap = false;
+    }
     void Turn()
     {
         transform.LookAt(transform.position + moveVec);
@@ -72,7 +112,7 @@ public class Player : MonoBehaviour
 
     void Dodge()
     {
-        if(spaceDown && !isDodge && !shiftDown)
+        if(spaceDown && !isDodge && !shiftDown &!isSwap)
         {
             dodgeVec = moveVec;
             speed *= 2;
@@ -92,7 +132,7 @@ public class Player : MonoBehaviour
 
     void Reload()
     {
-        if(rDown && moveVec == Vector3.zero && !isDodge)
+        if(rDown && moveVec == Vector3.zero && !isDodge & !isSwap)
         {
             anim.SetTrigger("doReload");
         }
@@ -102,5 +142,17 @@ public class Player : MonoBehaviour
         Camera.main.transform.parent = CameraTr.transform;
         Camera.main.transform.localPosition = Vector3.zero;
         Camera.main.transform.localRotation = Quaternion.identity;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Weapon"))
+        {
+            nearObject = other.gameObject;
+            Item item = nearObject.GetComponent<Item>();
+            int weaponIndex = item.value;
+            hasweapons[weaponIndex] = true;
+            Destroy(other.gameObject);
+        }
     }
 }
