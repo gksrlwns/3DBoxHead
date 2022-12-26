32c428f5dca1bcecff17c0f2c8506d2355c01158
gksrlwns <83814481+gksrlwns@users.noreply.github.com>
2022-12-26 18:54:46 +0900

    Photon Bug

--- Assets/Scripts/MenuSceneManager.cs
+++ Assets/Scripts/MenuSceneManager.cs
@@ -8,8 +8,8 @@ public class MenuSceneManager : MonoBehaviour
 {
     public GameObject title;
     public GameObject startBtn;
-    public GameObject soloBtn;
-    public GameObject coBtn;
+    public GameObject choiceObj;
+    public GameObject NickObj;
     Animation anim;
 
     private void Awake()
@@ -21,12 +21,17 @@ public class MenuSceneManager : MonoBehaviour
     {
         //타이틀과 이미지가 위로 올라가고 새로운 버튼 생성
         anim.Play();
-        startBtn.gameObject.SetActive(false);
-        soloBtn.SetActive(true);
-        coBtn.SetActive(true);
+        startBtn.SetActive(false);
+        choiceObj.SetActive(true);
     }
-    public void PlayBtn(int SceneIndex)
+    public void PlayBtn()
     {
-        SceneManager.LoadScene(SceneIndex);
+        SceneManager.LoadScene("SoloGameScene");
+    }
+
+    public void CoBtn()
+    {
+        choiceObj.SetActive(false);
+        NickObj.SetActive(true);
     }
 }
---
+++ Assets/Scripts/MultiGameManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class MultiGameManager : MonoBehaviour
+{
+    public Transform[] playerSpots;
+    private void Start()
+    {
+        var player = PhotonNetwork.Instantiate("PhotonPlayer", playerSpots[PhotonNetwork.CurrentRoom.PlayerCount].position, playerSpots[PhotonNetwork.CurrentRoom.PlayerCount].rotation);
+    }
+}
